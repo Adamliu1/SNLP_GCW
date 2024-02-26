0ed0455a4201aea74cc95df11d8c6d88dc0c5e12
Yadong LIU <yadonliu@mandarin.cs.ucl.ac.uk>
2024-02-26 17:37:27 +0000

    removed ref_model requirement. Updated cache_dir option for detect-pretrain-code_ucl

--- detect-pretrain-code_ucl/src/options.py
+++ detect-pretrain-code_ucl/src/options.py
@@ -17,6 +17,7 @@ class Options():
         self.parser.add_argument('--data', type=str, default="swj0419/WikiMIA", help="the dataset to evaluate: default is WikiMIA")
         self.parser.add_argument('--length', type=int, default=64, help="the length of the input text to evaluate. Choose from 32, 64, 128, 256")
         self.parser.add_argument('--key_name', type=str, default="input", help="the key name corresponding to the input text. Selecting from: input, parapgrase")
+        self.parser.add_argument('--cache_dir', type=str, default=".cache", help="the cache directory to store the dataset")
 
 
 
