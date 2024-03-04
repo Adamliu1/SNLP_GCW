bfc45b64e3918cdf6546a139701f9a8078d24acd
TheRootOf3 <aceszablewski@gmail.com>
2024-03-04 03:04:32 +0000

    Added a script to attach tokenizers to models and modified unlearn_harm.py.

---
+++ llm_unlearn_ucl/add_toknizers_to_models.py
@@ -0,0 +1,29 @@
+from transformers import AutoTokenizer
+import argparse
+
+
+def main(args) -> None:
+    tokenizer = AutoTokenizer.from_pretrained(args.model_name, cache_dir=args.cache_dir)
+    tokenizer.save_pretrained(args.model_save_path)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter
+    )
+    parser.add_argument(
+        "--model_name",
+        type=str,
+        required=True,
+        help="Name of the model from huggingface for which to download a tokenizer.",
+    )
+    parser.add_argument(
+        "--model_save_path",
+        type=str,
+        required=True,
+        help="Path to the dir of the local saved model to attach a tokenizer.",
+    )
+    parser.add_argument("--cache_dir", type=str, default=".cache")
+
+    args = parser.parse_args()
+    main(args)
