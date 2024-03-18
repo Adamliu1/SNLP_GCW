ce272eeed46bac2ad9ecf4776b0e6d54055acd98
Andrzej Szablewski <zcabasz@ucl.ac.uk>
2024-03-18 04:58:58 +0000

    Removed realtoxicityprompts. Changed cache directory, added request caching.

--- eval_framework_tasks/evaluate_model.sh
+++ eval_framework_tasks/evaluate_model.sh
@@ -15,7 +15,7 @@ EXPERIMENT_PATH=/SAN/intelsys/llm/aszablew/snlp/SNLP_GCW/eval_framework_tasks/$E
 
 RESULTS_PATH=$EXPERIMENT_PATH/results
 LOGS_PATH=$EXPERIMENT_PATH/logs
-CACHE_PATH=$EXPERIMENT_PATH/.cache_$MODEL_NAME
+CACHE_PATH=$EXPERIMENT_PATH/.cache/$MODEL_NAME/cache
 
 mkdir $EXPERIMENT_PATH
 mkdir $RESULTS_PATH
@@ -23,7 +23,7 @@ mkdir $LOGS_PATH
 
 # Tasks selection
 HF_LLM_LEADERBOARD_TASKS="arc_challenge,hellaswag,truthfulqa_mc2,mmlu,winogrande,gsm8k"
-TASKS_ADDITIONAL="toxigen,realtoxicityprompts"
+TASKS_ADDITIONAL="toxigen"
 TASKS=$HF_LLM_LEADERBOARD_TASKS,$TASKS_ADDITIONAL
 
 hostname
@@ -35,8 +35,8 @@ nohup $LAUNCHER -m lm_eval --model hf \
     --batch_size auto \
     --trust_remote_code True \
     --output_path $RESULTS_PATH/$MODEL_NAME/$MODEL_NAME.json \
-    --wandb_args project=snlp-lm-eval-harness,name=$EXPERIMENT_NAME-$MODEL_NAME &> $LOGS_PATH/$MODEL_NAME.log 
-
-    # --use_cache $CACHE_PATH 
+    --wandb_args project=snlp-lm-eval-harness,name=$EXPERIMENT_NAME-$MODEL_NAME \
+    --cache_requests "true" \
+    --use_cache $CACHE_PATH &> $LOGS_PATH/$MODEL_NAME.log 
 
 date
--- eval_framework_tasks/run_mathqa_experiment_toxicity.sh
+++ eval_framework_tasks/run_mathqa_experiment_toxicity.sh
@@ -4,7 +4,7 @@ BASE_PATH=/SAN/intelsys/llm/aszablew/snlp/SNLP_GCW/eval_framework_tasks
 
 source $BASE_PATH/../venv/bin/activate
 
-echo "You need to log in using huggingface-cli and make sure you have access to the toxigen dataset ()."
+echo "WARNING: You need to log in using huggingface-cli and make sure you have access to the toxigen dataset!"
 
 MODELS=$(ls /SAN/intelsys/llm/aszablew/snlp/SNLP_GCW/snlp-unlearned-models/models/opt1.3b_unlearned_mathqa)
 EXPERIMENT_NAME="math-qa-experiment-toxicity"
