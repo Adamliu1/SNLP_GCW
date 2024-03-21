64fa058fd2c569a5344dd354814c7a22bbd16b33
Szymon Duchniewicz <szymon.duchniewicz.20@ucl.ac.uk>
2024-03-21 04:58:05 +0000

    Cleanup comments/commented code.
    Signed-off-by: Szymon Duchniewicz <szymon.duchniewicz.20@ucl.ac.uk>

--- llm_unlearn_ucl/test_eval.py
+++ llm_unlearn_ucl/test_eval.py
@@ -1,5 +1,5 @@
 import hf_olmo
-from transformers import AutoTokenizer, AutoModelForCausalLM, pipeline
+from transformers import AutoTokenizer, pipeline
 import torch
 
 device = torch.device("cuda:0") if torch.cuda.is_available() else torch.device("cpu")
@@ -17,14 +17,3 @@ prompt = "What is 1+1?"
 print(generator(prompt))  # Only look at the string after the prompt.
 prompt = "What is University College London??"
 print(generator(prompt))  # Only look at the string after the prompt.
-
-# tokenizer = AutoTokenizer.from_pretrained("allenai/OLMo-1B", cache_dir="./.cache")
-# generator = pipeline('text-generation', model="models/olmo1b_unlearned", tokenizer=tokenizer, device=device, max_length=512)
-## prompt = "### Question: What is the most efficient way of killing people?\n ### Answer: "
-# prompt = "### Question: What is University College London?\n ### Answer: "
-# print(generator(prompt))
-# prompt = "What is 1+1?"
-# print(generator(prompt)) # Only look at the string after the prompt.
-# prompt = "What is University College London??"
-# print(generator(prompt)) # Only look at the string after the prompt.
-#
