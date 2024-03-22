5c87295888951547d34f90ba9dbe5efe8b7cfeaf
Davidyz <Davidyz@users.noreply.github.com>
2024-03-22 00:01:42 +0000

    :art: Format Python code with psf/black

--- mink_data_probability/src/eval.py
+++ mink_data_probability/src/eval.py
@@ -1,5 +1,6 @@
 import logging
-logging.basicConfig(level='ERROR')
+
+logging.basicConfig(level="ERROR")
 import numpy as np
 from tqdm import tqdm
 import json
@@ -10,42 +11,48 @@ import matplotlib
 import random
 
 
-matplotlib.rcParams['pdf.fonttype'] = 42
-matplotlib.rcParams['ps.fonttype'] = 42
+matplotlib.rcParams["pdf.fonttype"] = 42
+matplotlib.rcParams["ps.fonttype"] = 42
 
 
-matplotlib.rcParams['pdf.fonttype'] = 42
-matplotlib.rcParams['ps.fonttype'] = 42
+matplotlib.rcParams["pdf.fonttype"] = 42
+matplotlib.rcParams["ps.fonttype"] = 42
 
-# plot data 
+
+# plot data
 def sweep(score, x):
     """
     Compute a ROC curve and then return the FPR, TPR, AUC, and ACC.
     """
     fpr, tpr, _ = roc_curve(x, -score)
-    acc = np.max(1-(fpr+(1-tpr))/2)
+    acc = np.max(1 - (fpr + (1 - tpr)) / 2)
     return fpr, tpr, auc(fpr, tpr), acc
 
 
-def do_plot(prediction, answers, sweep_fn=sweep, metric='auc', legend="", output_dir=None):
+def do_plot(
+    prediction, answers, sweep_fn=sweep, metric="auc", legend="", output_dir=None
+):
     """
     Generate the ROC curves by using ntest models as test models and the rest to train.
     """
     fpr, tpr, auc, acc = sweep_fn(np.array(prediction), np.array(answers, dtype=bool))
 
     print(fpr)
-    low = tpr[np.where(fpr<.05)[0][-1]]
+    low = tpr[np.where(fpr < 0.05)[0][-1]]
     # bp()
-    print('Attack %s   AUC %.4f, Accuracy %.4f, TPR@5%%FPR of %.4f\n'%(legend, auc,acc, low))
+    print(
+        "Attack %s   AUC %.4f, Accuracy %.4f, TPR@5%%FPR of %.4f\n"
+        % (legend, auc, acc, low)
+    )
 
-    metric_text = ''
-    if metric == 'auc':
-        metric_text = 'auc=%.3f'%auc
-    elif metric == 'acc':
-        metric_text = 'acc=%.3f'%acc
+    metric_text = ""
+    if metric == "auc":
+        metric_text = "auc=%.3f" % auc
+    elif metric == "acc":
+        metric_text = "acc=%.3f" % acc
 
-    plt.plot(fpr, tpr, label=legend+metric_text)
-    return legend, auc,acc, low
+    plt.plot(fpr, tpr, label=legend + metric_text)
+    return legend, auc, acc, low
 
 
 def fig_fpr_tpr(all_output, output_dir):
@@ -58,40 +65,48 @@ def fig_fpr_tpr(all_output, output_dir):
             if ("raw" in metric) and ("clf" not in metric):
                 continue
             metric2predictions[metric].append(ex["pred"][metric])
-    
-    plt.figure(figsize=(4,3))
+
+    plt.figure(figsize=(4, 3))
     with open(f"{output_dir}/auc.txt", "w") as f:
         for metric, predictions in metric2predictions.items():
-            legend, auc, acc, low = do_plot(predictions, answers, legend=metric, metric='auc', output_dir=output_dir)
-            f.write('%s   AUC %.4f, Accuracy %.4f, TPR@0.1%%FPR of %.4f\n'%(legend, auc, acc, low))
+            legend, auc, acc, low = do_plot(
+                predictions, answers, legend=metric, metric="auc", output_dir=output_dir
+            )
+            f.write(
+                "%s   AUC %.4f, Accuracy %.4f, TPR@0.1%%FPR of %.4f\n"
+                % (legend, auc, acc, low)
+            )
 
     plt.semilogx()
     plt.semilogy()
-    plt.xlim(1e-5,1)
-    plt.ylim(1e-5,1)
+    plt.xlim(1e-5, 1)
+    plt.ylim(1e-5, 1)
     plt.xlabel("False Positive Rate")
     plt.ylabel("True Positive Rate")
-    plt.plot([0, 1], [0, 1], ls='--', color='gray')
-    plt.subplots_adjust(bottom=.18, left=.18, top=.96, right=.96)
+    plt.plot([0, 1], [0, 1], ls="--", color="gray")
+    plt.subplots_adjust(bottom=0.18, left=0.18, top=0.96, right=0.96)
     plt.legend(fontsize=8)
     plt.savefig(f"{output_dir}/auc.png")
 
 
 def load_jsonl(input_path):
-    with open(input_path, 'r') as f:
+    with open(input_path, "r") as f:
         data = [json.loads(line) for line in tqdm(f)]
     random.seed(0)
     random.shuffle(data)
     return data
 
+
 def dump_jsonl(data, path):
-    with open(path, 'w') as f:
+    with open(path, "w") as f:
         for line in tqdm(data):
             f.write(json.dumps(line) + "\n")
 
+
 def read_jsonl(path):
-    with open(path, 'r') as f:
+    with open(path, "r") as f:
         return [json.loads(line) for line in tqdm(f)]
+
 
 def convert_huggingface_data_to_list_dic(dataset, max_samples):
     all_data = []
@@ -99,7 +114,7 @@ def convert_huggingface_data_to_list_dic(dataset, max_samples):
     if len(dataset) > max_samples:
         total_samples = max_samples
     for i in range(total_samples):
-        #if (dataset[i]['label'] == 0):
+        # if (dataset[i]['label'] == 0):
         ex = dataset[i]
         all_data.append(ex)
     return all_data
