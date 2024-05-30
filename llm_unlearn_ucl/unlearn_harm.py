419635c25ba463a77379ad43c4ce015a543afcda
Zhe Yu <zcabzyu@ucl.ac.uk>
2024-05-30 20:13:54 +0100

    feat: add sail/symbolic-instruction-tuning for symbolic inference

--- llm_unlearn_ucl/unlearn_harm.py
+++ llm_unlearn_ucl/unlearn_harm.py
@@ -40,6 +40,7 @@ from utils import (
     compute_kl,
     create_mathqa_dataloader_from_dataset,
     create_pku_dataloader_from_dataset,
+    create_symbolic_dataloader_from_dataset,
     create_truthfulqa_dataloader,
     get_answer_loss,
     get_rand_ans_loss,
@@ -226,8 +227,8 @@ def main(args) -> None:
         args.samples_count % args.sequential == 0
     ), "samples_count should be divisible by number of splits for sequential learning (--sequential)."
     assert (
-        args.samples_count // args.sequential
-    ) % args.batch_size == 0, "samples in each 'sequence' (--samples_count / --sequential) should be a multiple of batch_size."
+        (args.samples_count // args.sequential) % args.batch_size == 0
+    ), "samples in each 'sequence' (--samples_count / --sequential) should be a multiple of batch_size."
     accelerator = Accelerator()  # accelerator precision can be specified if required.
     device = accelerator.device
 
@@ -312,11 +313,53 @@ def main(args) -> None:
         # ADDITONALLY: create_truthfulqa_dataloader() is also using this pattern!!!
         question_prefix_str = "### Question:"
         answer_prefix_str = "### Answer:"
+    elif args.unlearning_dataset == "sail/symbolic-instruction-tuning":
+        # filter entries with harmful responses and draw random samples from the remaining dataset.
+        full_bad_dataset = load_dataset(
+            "sail/symbolic-instruction-tuning", split="train"
+        )
+        if args.shuffle_seed:
+            # shuffle the dataset with a given seed for reproducibility
+            full_bad_dataset = full_bad_dataset.shuffle(seed=args.shuffle_seed)
+        if args.sequential > 0:
+            # NOTE: sequential/batch unlearning using sliced dataset.
+            train_bad_dataset = full_bad_dataset.select(range(args.samples_count))
+        else:
+            # NOTE: full dataset like bytedance.
+            train_bad_dataset = full_bad_dataset
 
+        Path(args.samples_save_dir).mkdir(exist_ok=True)
+        bad_sample_path = f"{args.samples_save_dir}/symbolic_{args.samples_count if args.sequential > 0 else 'full'}_samples.json"
+        with open(bad_sample_path, "w") as fin:
+            print(f"Writing symbolic samples to {bad_sample_path}")
+            json.dump(
+                [
+                    train_bad_dataset[i]
+                    for i in range(
+                        args.samples_count
+                        if args.sequential > 0
+                        else len(train_bad_dataset)
+                    )
+                ],
+                fin,
+            )
+
+        train_bad_loaders = create_symbolic_dataloader_from_dataset(
+            tokenizer,
+            train_bad_dataset,
+            batch_size=args.batch_size,
+            splits=max(args.sequential, 1),
+        )
+
+        # XXX: for now this is the prefix that is added before each q and answer,
+        # it is used by get_rand_ans_loss() to extract just the question part and
+        # add a random answer to it.
+        # !!!! Has additional sideffect of model unlearning this pattern!!!!
+        # ADDITONALLY: create_truthfulqa_dataloader() is also using this pattern!!!
+        question_prefix_str = "### Question:"
+        answer_prefix_str = "### Answer:"
     elif args.unlearning_dataset == "math_qa":
-        assert (
-            False
-        ), "Mathqa temporarirly disabled - requries implementing returning a List of Datasets for sequential unlearning with equal sizes!"
+        assert False, "Mathqa temporarirly disabled - requries implementing returning a List of Datasets for sequential unlearning with equal sizes!"
         train_dataset = load_dataset("math_qa", split="train")
         train_bad_loader = create_mathqa_dataloader_from_dataset(
             tokenizer, train_dataset, batch_size=args.batch_size
@@ -425,9 +468,7 @@ def main(args) -> None:
     idx = 0  # Number of "unlearning steps" that has occurred (e.g. processed batches)
     samples_count = 0  # Total number of samples that "passed through" the model (for 1 pass of batch 32: 32 samples)
     start_time = time.time()  # Start time of unlearning process
-    running_loss = (
-        deque()
-    )  # averaging running value of "bad loss", used in ByteDance paper unlearning method
+    running_loss = deque()  # averaging running value of "bad loss", used in ByteDance paper unlearning method
     final_model_tag = 0
     # Here for caching what samples are used so far
 
