57f25f3e1bd1334a64b282460a89cecc5aa83d7d
Adamliu1 <liu0122@outlook.com>
2024-06-07 17:22:30 +0100

    update scripts for redoing beavertail exps

--- eval_harmfulness/run_experiment_multiple_seed.sh
+++ eval_harmfulness/run_experiment_multiple_seed.sh
@@ -1,45 +1,44 @@
 #!/bin/bash
 
 # Experiment config
-MODEL_NAME=test-llama3
-# NOTE: here I will modify to run with different seeds
-EXPERIMENT_NAME=$MODEL_NAME-test
-
-
+MODEL_NAME=llama3
 
 # Paths and directories
-# MODELS_PATH=/SAN/intelsys/llm/aszablew/snlp/SNLP_GCW/snlp-unlearned-models/models/$MODEL_NAME
 MODELS_PATH="meta-llama/Meta-Llama-3-8B"
 BASE_PATH=/cs/student/ug/2020/yadonliu/my_fyp_space/SNLP_GCW/eval_harmfulness
-# source $BASE_PATH/../venv/bin/activate
 
 # Environment config
 DEVICE="cuda:0"
+MODEL="llama3"
 
-# Generate model outputs for each question in the dataset
-# MODELS=$(ls $MODELS_PATH)
+seeds=(42 123 456 114514 8888)
 
 hostname
 
-# for model in ${MODELS[@]}
-# do 
+for seed in ${seeds[@]}
+do 
+    EXPERIMENT_NAME="${MODEL_NAME}_eval/seed_${seed}"
     date
-    echo "Generating model answers for $model..."
+    echo "Generating model answers for $MODEL..."
     python3 beavertails_get_model_answers.py \
         --model_path $MODELS_PATH \
         --device $DEVICE \
-        --batch_size 64 \
+        --batch_size 8 \
+        --seed $seed \
         --output_dir "$BASE_PATH/model_generations/$EXPERIMENT_NAME"
 
-# done
+done
 
-# Evaluate all generations
-date
-nohup python evaluate_outputs.py \
-    --eval_dataset "$BASE_PATH/model_generations/$EXPERIMENT_NAME/" \
-    --model_path "PKU-Alignment/beaver-dam-7b" \
-    --max_length 512 \
-    --output_dir "$BASE_PATH/eval_results/$EXPERIMENT_NAME" \
-    --device $DEVICE
-    
-# date
+echo "Evaluating model answers"
+for seed in ${seeds[@]}
+do
+    # Evaluate all generations
+    EXPERIMENT_NAME="${MODEL_NAME}_eval/seed_${seed}"
+    date
+    python evaluate_outputs.py \
+        --eval_dataset "$BASE_PATH/model_generations/$EXPERIMENT_NAME/" \
+        --model_path "PKU-Alignment/beaver-dam-7b" \
+        --max_length 512 \
+        --output_dir "$BASE_PATH/eval_results/$EXPERIMENT_NAME" \
+        --device $DEVICE
+done
