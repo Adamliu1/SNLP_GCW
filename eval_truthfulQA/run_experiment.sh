89c04c3b63d8e1612b2733e16f3ec84c091cc302
Andrzej Szablewski <zcabasz@ucl.ac.uk>
2024-08-29 23:57:02 +0100

    Added run_experiment.sh script for truthfulqa eval.
    Signed-off-by: Andrzej Szablewski <zcabasz@ucl.ac.uk>

--- eval_truthfulQA/run_experiment.sh
+++ eval_truthfulQA/run_experiment.sh
@@ -12,7 +12,7 @@ BASE_PATH=/SAN/intelsys/llm/aszablew/snlp/SNLP_GCW/eval_harmfulness
 source $BASE_PATH/../venv/bin/activate
 
 # Environment config
-DEVICE="cuda:1"
+DEVICE="cuda:3"
 
 # Generate model outputs for each question in the dataset
 MODELS=$(ls $MODELS_PATH)
@@ -24,10 +24,10 @@ for model in ${MODELS[@]}
 do 
     date
     echo "Generating model answers for $model..."
-    nohup python3 beavertails_get_model_answers.py \
+    nohup python3 generate_outputs.py \
         --model_path $MODELS_PATH/$model \
         --device $DEVICE \
-        --batch_size 128 \ 
+        --batch_size 128 \
         --output_dir "$BASE_PATH/model_generations/$EXPERIMENT_NAME"
 
 done
@@ -36,9 +36,6 @@ done
 date
 nohup python evaluate_outputs.py \
     --eval_dataset "$BASE_PATH/model_generations/$EXPERIMENT_NAME/" \
-    --model_path ./beaver-dam-7_weights \
-    --max_length 512 \
     --output_dir "$BASE_PATH/eval_results/$EXPERIMENT_NAME" \
-    --device $DEVICE
     
 date
